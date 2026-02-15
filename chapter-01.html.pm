ad6041ddc497021b863050bdd5ff7222ce0188aa
therapy-rgb <231003516+therapy-rgb@users.noreply.github.com>
2026-02-14 20:25:04 -0500

    2

--- chapter-01.html.pm
+++ chapter-01.html.pm
@@ -6,7 +6,7 @@
 
 ◊chapter-title{Getting Started}
 
-Aristotle was big on finding a starting point. 
+Aristotle was big on finding a starting point. That sounds simple, but we live always in the middle of things, never fully able to step outside of circumstances. 
 
 
 
